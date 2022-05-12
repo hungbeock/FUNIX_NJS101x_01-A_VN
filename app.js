bfe812da202b11baad44a3e6c5e5bda60cf6bef8
hungbeock <hungbeock2@gmail.com>
2022-05-12 22:25:23 +0900

    LAP2_12

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const bodyParser = require('body-parser')
 const app = express()
 const adminRouter= require('./routes/admin')
 const shopRouter= require('./routes/shop')
-
+const path = require('path')
 app.use(bodyParser.urlencoded({extended:false }))
 
 app.use( '/admin', adminRouter)
 app.use(shopRouter)
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>')
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'))
 })
 
 const server= http.createServer(app)
