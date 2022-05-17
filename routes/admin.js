3f192217ce125f4eb09f229619b00d2eea1d6613
hungbeock <hungbeock2@gmail.com>
2022-05-17 22:54:30 +0900

    LAP3_4

--- routes/admin.js
+++ routes/admin.js
@@ -7,8 +7,9 @@ const products =[]
 const router = express.Router();
 //admin/add-product =>GET
 router.get('/add-product', ( req, res, next) => {
-   
-    res.sendFile(path.join(rootDir, 'views','add-product.html'))
+    // res.sendFile(path.join(rootDir, 'views','add-product.html'))
+    res.render('add-product',{pageTitle :'Add Product'})
+    // res.render('add-product',{prods:products,docTitle:'Shop    '})
 })
 //admin/add-product => POST
 router.post('/add-product',( req, res, next)=>{
