f67a268facb0579cf93f17eecd5070a1a7a7c642
hungbeock <hungbeock2@gmail.com>
2022-05-17 23:05:01 +0900

    LAP3_4

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use( '/admin', adminData.routes)
 app.use(shopRouter)
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'))
+    res.status(404).render('404')
 })
 
 const server= http.createServer(app)
