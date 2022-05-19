9558dfeba564911a036acdb076d2649813900a6d
hungbeock <hungbeock2@gmail.com>
2022-05-19 20:54:30 +0900

    LAP4_5

--- models/product.js
+++ models/product.js
@@ -12,31 +12,29 @@ module.exports= class Product{
             'data',
             'products.json'
             );
-            fs.readFile(p,(err,fileContent)=>{
-                let products=[];
-                if(!err){
-                    products=JSON.parse(fileContent)   
+            fs.readFile(p, (err, fileContent) => {
+                let products = [];
+                if (!err) {
+                  products = JSON.parse(fileContent);
                 }
-                products.push(this)
-                fs.writeFile(p,JSON.stringify(products),(err)=>{
-                    console.log(err)
-                })
-            })
+                products.push(this);
+                fs.writeFile(p, JSON.stringify(products), err => {
+                  console.log(err);
+                });
+              });
     }
 
-    static fetchAll(){
+    static fetchAll(cb) {
         const p = path.join(
-            path.dirname(require.main.filename),
-            'data',
-            'products.json'
-            )
-        fs.readFile(p ,(err,fileContent)=>{
-            if(err){
-                return []
-            }
-            return JSON.parse(fileContent)
-        })
-        return products
-    }
-
-}
+          path.dirname(process.mainModule.filename),
+          'data',
+          'products.json'
+        );
+        fs.readFile(p, (err, fileContent) => {
+          if (err) {
+            cb([]);
+          }
+          cb(JSON.parse(fileContent));
+        });
+      }
+    };
