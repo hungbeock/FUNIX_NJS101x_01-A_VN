ab6142c581887c9cc47aa57a6e888d643793377a
hungbeock <hungbeock2@gmail.com>
2022-05-27 21:55:26 +0900

    LAP4_20

--- models/cart.js
+++ models/cart.js
@@ -38,32 +38,35 @@ module.exports = class Cart {
     });
   }
 
-  static deleteProduct(id ,productPrice) {
+  static deleteProduct(id, productPrice) {
     fs.readFile(p, (err, fileContent) => {
-        if (err){
-          return
-        }
-        const updatedCart ={...JSON.parse(fileContent)}
-        const product=updatedCart.products.find(prod=> prod.id === id)
-        const productQty= product.qty
-        updatedCart.products=updatedCart.products.filter(prod=> prod.id !== id)
-        updatedCart.totalPrice=updatedCart.totalPrice- productPrice*productQty
-        fs.writeFile(p, JSON.stringify(cart), err => {
-          console.log(err);
-        });
-      })
+      if (err) {
+        return;
+      }
+      const updatedCart = { ...JSON.parse(fileContent) };
+      const product = updatedCart.products.find(prod => prod.id === id);
 
+      const productQty = product.qty;
+      updatedCart.products = updatedCart.products.filter(
+        prod => prod.id !== id
+      );
+      updatedCart.totalPrice =
+        updatedCart.totalPrice - productPrice * productQty;
+
+      fs.writeFile(p, JSON.stringify(updatedCart), err => {
+        console.log(err);
+      });
+    });
   }
 
-  static getCart(cb){
+  static getCart(cb) {
     fs.readFile(p, (err, fileContent) => {
-      const cart = JSON.parse(fileContent)
-      if(err){
-        cb(null)
-      }else{
-        cb(cart)
+      const cart = JSON.parse(fileContent);
+      if (err) {
+        cb(null);
+      } else {
+        cb(cart);
       }
-     
-    })
+    });
   }
 };
