7b1e885bf0ec47bd9b7013253c9adb6407d87af7
hungbeock <hungbeock2@gmail.com>
2022-06-01 06:51:50 +0900

    LAP5_3

--- util/database.js
+++ util/database.js
@@ -4,7 +4,7 @@ const pool =mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'node-complete', 
-    password: 'nodecomplete'
+    password: '123456789'
 })
 
 module.exports =pool.promise()
