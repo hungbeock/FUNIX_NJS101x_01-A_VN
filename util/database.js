8b708ba76d20a7d6792410f6238c9d58bb832179
hungbeock <hungbeock2@gmail.com>
2022-06-02 05:48:24 +0900

    LAP5_10

--- util/database.js
+++ util/database.js
@@ -1,8 +1,8 @@
-const Sequelize =require(' sequelize')
+const Sequelize = require('sequelize');
 
-const sequelize= new Sequelize('node-complete' ,'root','nodecomplete',{
-    dialect: 'mysql',
-    host: 'localhost'
-})
+const sequelize = new Sequelize('node-complete', 'root', '123456789', {
+  dialect: 'mysql',
+  host: 'localhost'
+});
 
-module.exports = sequelize
+module.exports = sequelize;
