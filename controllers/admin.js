e90b39920beb988420cefd5a3582351cf5b4967a
hungbeock <hungbeock2@gmail.com>
2022-06-01 22:24:41 +0900

    LAP5_6

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,15 +1,9 @@
 const Product = require('../models/product');
 
-
-exports.getAddProduct = async (req, res, next) => {
-
-  const productEx ={"id":"152","title":"sach1","imageUrl":"sag","description":"hay","price":"34"}
-
-
+exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    product: productEx,
     editing: false
   });
 };
@@ -19,9 +13,13 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null,title, imageUrl, description, price);
-  product.save();
-  res.redirect('/');
+  const product = new Product(null, title, imageUrl, description, price);
+  product
+    .save()
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch(err => console.log(err));
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -60,7 +58,6 @@ exports.postEditProduct = (req, res, next) => {
   res.redirect('/admin/products');
 };
 
-
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('admin/products', {
@@ -71,8 +68,8 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
-exports.postDeleteProduct =(req, res, next)=>{
+exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-    Product.deleteById(prodId)
-    res.redirect('/admin/products')
-}
+  Product.deleteById(prodId);
+  res.redirect('/admin/products');
+};
--- models/product.js
+++ models/product.js
@@ -1,6 +1,6 @@
-const db=require('../util/database')
+const db = require('../util/database');
 
-const Cart = require('./cart')
+const Cart = require('./cart');
 
 module.exports = class Product {
   constructor(id, title, imageUrl, description, price) {
@@ -12,18 +12,17 @@ module.exports = class Product {
   }
 
   save() {
-   
+    return db.execute(
+      'INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
+      [this.title, this.price, this.imageUrl, this.description]
+    );
   }
 
-  static deleteById(id){
-   
-  }
+  static deleteById(id) {}
 
   static fetchAll() {
-    return  db.execute('select * from products')
+    return db.execute('SELECT * FROM products');
   }
 
-  static findById(id) {
- 
-  }
+  static findById(id) {}
 };
