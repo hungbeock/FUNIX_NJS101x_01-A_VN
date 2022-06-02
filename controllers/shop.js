db724a00479d281ca5488a8a62518d68587bdf92
hungbeock <hungbeock2@gmail.com>
2022-06-02 23:18:14 +0900

    LAP5_27

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,6 @@
 const Product = require('../models/product');
 const Cart =require('../models/cart')
-
+const Order = require('../models/order')
 exports.getProducts = (req, res, next) => {
   Product
   .findAll()
@@ -157,6 +157,36 @@ exports.postCartDeleteProduct = (req, res, next) => {
     })
     .then(result => {
       res.redirect('/cart');
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postOrder = (req, res, next) => {
+  let fetchedCart;
+  req.user
+    .getCart()
+    .then(cart => {
+      fetchedCart = cart;
+      return cart.getProducts();
+    })
+    .then(products => {
+      return req.user
+        .createOrder()
+        .then(order => {
+          return order.addProducts(
+            products.map(product => {
+              product.orderItem = { quantity: product.cartItem.quantity };
+              return product;
+            })
+          );
+        })
+        .catch(err => console.log(err));
+    })
+    .then(result => {
+      return fetchedCart.setProducts(null);
+    })
+    .then(result => {
+      res.redirect('/orders');
     })
     .catch(err => console.log(err));
 };
