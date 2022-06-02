25291122a14b62e4f8a4d18526832d91bd7b6690
hungbeock <hungbeock2@gmail.com>
2022-06-02 22:31:39 +0900

    LAP5_22

--- app.js
+++ app.js
@@ -46,8 +46,8 @@ Cart.belongsToMany(Product,{through :CartItem})
 Product.belongsToMany(Cart,{through :CartItem})
 
 sequelize
-.sync({force : true})
-// .sync()
+// .sync({force : true})
+.sync()
 .then(result => {
     return  User.findById(1)
     console.log(result);
@@ -63,7 +63,11 @@ sequelize
 )
 .then(user => {
     // console.log(user)
+  return  user.createCart()
+})
+.then(cart => {
     app.listen(3000);
+
 })
 .catch(err => {
     console.log(err);
