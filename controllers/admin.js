a50078c488164067f8ce87ba305d902b14467755
hungbeock <hungbeock2@gmail.com>
2022-06-02 21:04:00 +0900

    LAP5_16

--- controllers/admin.js
+++ controllers/admin.js
@@ -23,6 +23,7 @@ exports.postAddProduct = (req, res, next) => {
       // console.log(result);
       //dalam
       console.log('Created Product');
+      res.redirect('/admin/products')
     })
     .catch(err => {
       console.log(err);
@@ -93,6 +94,17 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect('/admin/products');
+  Product.findById(prodId)
+  .then(
+    product =>{
+     return product.destroy()
+    }
+  )
+  .then(
+    result =>{
+      console.log('DESTROYED PRODUCT')
+      res.redirect('/admin/products');
+    }
+  )
+  .catch(err => {console.log(err);})
 };
