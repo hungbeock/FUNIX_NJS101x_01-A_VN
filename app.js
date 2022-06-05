5d7829d7fab08b166c9dfe85e224084f4876624c
hungbeock <hungbeock2@gmail.com>
2022-06-05 21:21:49 +0900

    LAP6_3

--- app.js
+++ app.js
@@ -33,8 +33,8 @@ app.use('/admin', adminRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect((client)=>{
-    console.log(client)
+mongoConnect(()=>{
+    
     app.listen(3000)
 })
 
--- util/database.js
+++ util/database.js
@@ -2,16 +2,30 @@ const mongodb = require('mongodb');
 
 const MongoClient =mongodb.MongoClient
 
+let _db
 const mongoConnect =(callback)=>{
 
   MongoClient.connect(
-    'mongodb+srv://test:123456789a@cluster0.sle1q.mongodb.net/?retryWrites=true&w=majority'
+    'mongodb+srv://test:123456789a@cluster0.sle1q.mongodb.net/shop?retryWrites=true&w=majority'
     )
   .then(client => {
     console.log('Connected')
+    _db=client.db()
     callback(client)
   })
-  .catch(err => console.log(err))
+  .catch(
+    err => {
+      console.log(err)
+      throw err
+    })
 }
 
-module.exports = mongoConnect
+const getDb=() => {
+  if(_db){
+    return _db
+  }
+  throw 'No database found'
+}
+
+exports.mongoConnect  =mongoConnect
+exports.getDb=getDb
