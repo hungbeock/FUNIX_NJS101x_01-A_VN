9ca18aec31902d5ddf62f3503b7d2411c05641b4
hungbeock <hungbeock2@gmail.com>
2022-06-06 20:35:32 +0900

    LAP6_7

--- controllers/shop.js
+++ controllers/shop.js
@@ -33,28 +33,24 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findAll({where: {id : prodId}})
-  .then(products=>{
-    res.render('shop/product-detail', {
-      product: products[0],
-      pageTitle: products[0].title,
-      path: '/products',
-    })
-  })
-  .catch(err => {console.error(err)})
-  // Product
-  // .findById(prodId)
-  // .then( product=> {
-
-  //     console.log('product', product);
+  // Product.findAll({ where: { id: prodId } })
+  //   .then(products => {
   //     res.render('shop/product-detail', {
-  //       product: product,
-  //       pageTitle: product.title,
-  //       path: '/products',
-  //     })
-    
-  // })
-  // .catch(err => console.log(err));
+  //       product: products[0],
+  //       pageTitle: products[0].title,
+  //       path: '/products'
+  //     });
+  //   })
+  //   .catch(err => console.log(err));
+  Product.findById(prodId)
+    .then(product => {
+      res.render('shop/product-detail', {
+        product: product,
+        pageTitle: product.title,
+        path: '/products'
+      });
+    })
+    .catch(err => console.log(err));
 };
 
 
