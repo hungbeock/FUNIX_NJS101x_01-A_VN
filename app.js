103614898b49cdc383437deb023d8599a2e1ed20
hungbeock <hungbeock2@gmail.com>
2022-06-06 21:42:24 +0900

    LAP6_13

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const mongoConnect= require('./util/database').mongoConnect
+const User = require('./models/user')
 
 const app = express();
 
@@ -20,12 +21,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    // User.findById(1)
-    // .then(user => {
-    //     req.user=user;
-    //     next();
-    // })
-    // .catch((err) => {console.log(err)})
+    User.findById('629df5a9bdbb720e9ae7747d')
+    .then(user => {
+        req.user=user;
+        next();
+    })
+    .catch((err) => {console.log(err)})
     next();
 })
 
