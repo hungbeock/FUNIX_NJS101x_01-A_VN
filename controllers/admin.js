fc5bae9fa6daa155cdec8d9a41a73ed2d64b7bd3
hungbeock <hungbeock2@gmail.com>
2022-06-08 20:57:59 +0900

    LAP7_6

--- controllers/admin.js
+++ controllers/admin.js
@@ -39,7 +39,7 @@ exports.getEditProduct = (req, res, next) => {
   }
   const prodId = req.params.productId;
 Product.findById(prodId)
-  // Product.findById(prodId)
+
   .then(
     product => {
       if (!product) {
@@ -63,18 +63,14 @@ exports.postEditProduct = (req, res, next) => {
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
 
-
-      const product = new Product(
-        updatedTitle,
-        updatedPrice,
-        updatedDesc,
-        updatedImageUrl,
-        prodId
-        )
-     
-    
-  product
-  .save()
+  Product.findById(prodId).then(product => {
+    product.title = updatedTitle ,
+    product.price = updatedPrice ,
+    product.description = updatedDesc,
+    product.imageUrl = updatedImageUrl
+    return product
+    .save()
+  })
   .then(result=>{
     console.log('UPDATED PRODUCT')
     res.redirect('/admin/products');
@@ -83,7 +79,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
+  Product.find()
   .then(
     products => {
       res.render('admin/products', {
