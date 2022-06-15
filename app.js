cbec3c00580a3183784d167b9228da946b980d94
hungbeock <hungbeock2@gmail.com>
2022-06-15 22:11:18 +0900

    THem file

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-
+const authRoutes =require('./routes/auth')
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -31,6 +31,8 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
+
 
 app.use(errorController.get404);
 
