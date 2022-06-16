5f7b09aae45e6ad2d8c83bd2a90fb227cba55dd9
hungbeock <hungbeock2@gmail.com>
2022-06-16 18:28:12 +0900

    LAP8_4

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,20 @@
 exports.getLogin = (req, res, next) => {
-  const  isLoggedIn = req
-  .get('Cookie')
-  .split(';')[0]
-  .trim()
-  .split('=')[1] === 'true'
-  console.log(isLoggedIn)
+  // const  isLoggedIn = req
+  // .get('Cookie')
+  // .split(';')[0]
+  // .trim()
+  // .split('=')[1] === 'true'
+  // console.log(isLoggedIn)
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login', 
-    isAuthenticated : isLoggedIn
+    isAuthenticated : false
 
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader('Set-Cookie', 'loggedIn=true')
+  res.setHeader('Set-Cookie', 'loggedIn=true; HttpOnly ')
   res.redirect('/')
 };
 
