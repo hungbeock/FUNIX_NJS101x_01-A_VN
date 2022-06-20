c8016a95a0cacabba18b6745e924788e665cb5e9
hungbeock <hungbeock2@gmail.com>
2022-06-20 21:43:04 +0900

    LAP9_15

--- controllers/error.js
+++ controllers/error.js
@@ -6,4 +6,14 @@ exports.get404 = (req, res, next) => {
 
     });
   };
+
+  exports.get500 = (req, res, next) => {
+    res.status(500).render('500', 
+    { pageTitle: 'Error',
+     path: '/500' ,
+     isAuthenticated : req.isLoggedIn
+
+    });
+  };
+  
   
