761160258cdc7b2a10cfd9e63c41167acf8c71fb
hungbeock <hungbeock2@gmail.com>
2022-06-20 21:52:48 +0900

    LAP9_16

--- app.js
+++ app.js
@@ -72,11 +72,16 @@ app.use(authRoutes);
 app.use('/500',errorController.get500);
 app.use(errorController.get404);
 
+app.use((error,req, res, next)=>{
+  res.redirect('/500');
+})
+
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
     app.listen(3000);
   })
   .catch(err => {
+    //res.status(error.httpStatusCode).render('....')
     console.log(err);
   });
--- controllers/admin.js
+++ controllers/admin.js
@@ -70,7 +70,10 @@ exports.postAddProduct = (req, res, next) => {
       //   errorMessage: 'Database operation  failed please try again.',
       //   validationErrors: []
       // });
-      res.redirect('/500')
+      // res.redirect('/500')
+      const error = new Error(err);
+      error.httpStatusCode =500
+      return next(error);
     });
 };
 
@@ -82,6 +85,7 @@ exports.getEditProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId)
     .then(product => {
+  
       if (!product) {
         return res.redirect('/');
       }
@@ -95,7 +99,11 @@ exports.getEditProduct = (req, res, next) => {
         validationErrors: []
       });
     })
-    .catch(err => console.log(err));
+    .catch(err => 
+     { const error = new Error(err);
+      error.httpStatusCode =500
+      return next(error);}
+      );
 };
 
 exports.postEditProduct = (req, res, next) => {
