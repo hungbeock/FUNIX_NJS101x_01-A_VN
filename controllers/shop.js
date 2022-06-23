570034e2fc6f21d160b2ead18318b92073b0d281
hungbeock <hungbeock2@gmail.com>
2022-06-23 22:40:56 +0900

    LAP10_14

--- controllers/shop.js
+++ controllers/shop.js
@@ -160,16 +160,37 @@ exports.getInvoice = (req, res, next) => {
       const invoiceName = 'invoice-' + orderId + '.pdf';
       const invoicePath = path.join('data', 'invoices', invoiceName);
 
-      const pdfDoc= new PDFDocument()
+      const pdfDoc = new PDFDocument();
       res.setHeader('Content-Type', 'application/pdf');
       res.setHeader(
         'Content-Disposition',
         'inline; filename="' + invoiceName + '"'
       );
-      pdfDoc.pipe(fs.createWriteStream(invoicePath))
-      pdfDoc.pipe(res)
-      pdfDoc.text('Hello ae')
-      pdfDoc.end()
+      pdfDoc.pipe(fs.createWriteStream(invoicePath));
+      pdfDoc.pipe(res);
+
+      pdfDoc.fontSize(26).text('Invoice', {
+        underline: true
+      });
+      pdfDoc.text('-----------------------');
+      let totalPrice = 0;
+      order.products.forEach(prod => {
+        totalPrice += prod.quantity * prod.product.price;
+        pdfDoc
+          .fontSize(14)
+          .text(
+            prod.product.title +
+              ' - ' +
+              prod.quantity +
+              ' x ' +
+              '$' +
+              prod.product.price
+          );
+      });
+      pdfDoc.text('---');
+      pdfDoc.fontSize(20).text('Total Price: $' + totalPrice);
+
+      pdfDoc.end();
       // fs.readFile(invoicePath, (err, data) => {
       //   if (err) {
       //     return next(err);
@@ -182,7 +203,7 @@ exports.getInvoice = (req, res, next) => {
       //   res.send(data);
       // });
       // const file = fs.createReadStream(invoicePath);
- 
+
       // file.pipe(res);
     })
     .catch(err => next(err));
