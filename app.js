3500abdbd07247b28e3eecd958fa9dc33de4490c
hungbeock <hungbeock2@gmail.com>
2022-06-23 20:59:50 +0900

    LAP10_6

--- app.js
+++ app.js
@@ -24,24 +24,26 @@ const store = new MongoDBStore({
 
 const csrfProtection = csrf()
 
-const fileStorage= multer.diskStorage({
-  destination: (req,file,cb)=>{
-    cb(null,'images')
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
   },
-  filename: (req,file,cb)=>{
-    cb(null, new Date().toISOString() +'-' +file.originalname)
+  filename: (req, file, cb) => {
+    cb(null,  file.originalname);
   }
-})
+});
 
-const fileFilter = (req,file,cb)=>{
-  if(file.mimetype ==='image/png' || file .mimetype==='image/jpg' || file .mimetype==='image/jpeg'){
-
-    cb(null,true ) 
-  }else {
-
-    cb(null,false )
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
   }
-}
+};
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -51,7 +53,9 @@ const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({storage :fileStorage , fileFilter:fileFilter }).single('image'))
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   session({
